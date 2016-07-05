2cd1a49783ac54dc0adf1142161ac7d2f67d350f
renpeng <931322306@qq.com>
2016-07-05 14:06:37 +0800

    start

---
+++ fengx/src/main/java/com/fengx/FengxApplication.java
@@ -0,0 +1,12 @@
+package com.fengx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FengxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FengxApplication.class, args);
+	}
+}
